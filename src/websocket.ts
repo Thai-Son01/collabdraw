9d397e30f2ea15f0f4ae803a95f34d74a8f6ad4d
Thai-Son01 <cthaison01@gmail.com>
2025-03-18 16:42:04 -0400

    will need to fix canvas size. also have to start syncing data

--- src/websocket.ts
+++ src/websocket.ts
@@ -23,32 +23,12 @@ export default function connectToWebSocket(userId : string, room : string, serve
 
         onConnect: () => {
 
-            //testing purpose
-            client.subscribe("/topic/greetings", message => {
-            console.log(message);
-            });
-
-
-            // console.log(client.connected);
-
-            // client.publish({destination : "/app/hello",
-            // body: JSON.stringify({'name': userId})
-            //     });
             client.publish({destination : "/app/connect/" + room,
                     body: JSON.stringify({'name': userId})
                         });
-
-            // console.log(`/user/${userId}/queue/${room}`);
-            // client.subscribe(`/user/queue/${room}`, message => {
-            //     console.log("THIS SHOULD WORK CMON MAN")
-            //     console.log(message);
-            // })
-
             console.log("connected to server through websocket");
 
             }
-
-            //testing purpose
 
         })
     client.activate()
